0a22f98001d3d250a2e65c6f9b53efd7cf108371
CYAN4S <37798132+CYAN4S@users.noreply.github.com>
2021-02-16 22:57:57 +0900

    ui practice

--- src/App.tsx
+++ src/App.tsx
@@ -1,16 +1,30 @@
-import React from "react";
+import React, { useState } from "react";
 import logo from "./logo.svg";
 import "./App.css";
+import Radio from "./components/Radio";
 
 import DescEditor from "./components/DescEditor";
 import ChartEditor from "./components/ChartEditor";
 
 function App() {
+  const [state, setState] = useState("Desc");
+  const radioenv = {
+    name: "state",
+    onChange: (ev: React.ChangeEvent<HTMLInputElement>) => {
+      setState(ev.target.value);
+    },
+    state: state,
+  };
+
   return (
-    <div className="App">
+    <div className="App" style={{display: "flex", gap: "20px"}}>
+      <nav>
+        <Radio env={radioenv} value="Desc" /> Desc
+        <br />
+        <Radio env={radioenv} value="Chart" /> Chart
+      </nav>
       <div className="editors">
-        <DescEditor />
-        <ChartEditor />
+        {state == "Desc" ? <DescEditor /> : "Chart" ? <ChartEditor /> : <> </>}
       </div>
     </div>
   );
